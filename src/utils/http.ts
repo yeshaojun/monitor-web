af91b7aa0b975c79fc74034f3f477e50cf7dfc3f
yeshaojun <sjye@rsvptech.cn>
2023-05-08 17:09:29 +0800

    fix:add

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,8 +2,9 @@ import { createFetch } from '@vueuse/core'
 import { useLocalStorage } from '@vueuse/core'
 import { ElMessage } from 'element-plus'
 
+const base = 'http://api.yeshaojun.com/v1/' //'http://localhost:9000/v1/'
 const useMyFetch = createFetch({
-  baseUrl: 'http://localhost:9000/v1/',
+  baseUrl: base,
   options: {
     onFetchError(ctx) {
       console.log('ctx', ctx)
